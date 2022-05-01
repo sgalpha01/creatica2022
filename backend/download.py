ee68aaaafe04f7c809c8a9f974ae71d3f57db853
Dhakshinesh <dhakshineshanandh@gmail.com>
2022-05-01 12:09:46 +0530

    Added function : Downloads images scraped from a website.

---
+++ backend/download.py
@@ -0,0 +1,28 @@
+import requests 
+from bs4 import BeautifulSoup 
+import urllib.request
+
+    
+def getdata(url): 
+    r = requests.get(url) 
+    return r.text 
+    
+def get_image_list(url):
+    htmldata = getdata(url) 
+    soup = BeautifulSoup(htmldata, 'html.parser') 
+    urls = []
+    for item in soup.find_all('img'):
+        urls.append(item['src'])
+    download_images_function(urls)
+
+def download_image(url, file_path, file_name):
+        full_path = file_path + file_name + '.png'
+        urllib.request.urlretrieve(url, full_path)
+
+def download_images_function(urls):
+    for url in urls:
+        file_name = str(id(url))
+        try:
+            download_image(url, 'backend/temp_images/', file_name)
+        except:
+            pass
--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from download import get_image_list
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware)
-
+#retrieves/downloads images from a website
+get_image_list("https://www.geeksforgeeks.org/")
 
 @app.post("/api/url")
 async def scan_url(url: str):
