33711c4a732ef7f74d7fae259a256efa1cab3b42
Suyash Gupta <sgalpha01@gmail.com>
2022-05-01 16:19:16 +0530

    refactored code

--- backend/main.py
+++ backend/main.py
@@ -1,30 +1,23 @@
+import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-import uvicorn
-from download import get_image_list
+from fastapi.templating import Jinja2Templates
+
+from image_downloader import get_image_list
+from nsfw_detector import results
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware)
 templates = Jinja2Templates(directory="../frontend/templates")
-app.mount("/static", StaticFiles(directory="../frontend/static"),name="static")
-
-# retrieves/downloads images from a website
-def request_download_image(url):
-    get_image_list(url)
+app.mount("/static", StaticFiles(directory="../frontend/static"), name="static")
 
 
-#Custom url
-@app.get('/', response_class=HTMLResponse)
+# Landing Page
+@app.get("/", response_class=HTMLResponse)
 async def hello_world(request: Request):
-    return templates.TemplateResponse("Custom.html", {"request" : request})
-
-#Website-1
-@app.get('/website-1', response_class=HTMLResponse)
-async def hello_world(request: Request):
-    return templates.TemplateResponse("Home.html", {"request": request})
+    return templates.TemplateResponse("Custom.html", {"request": request})
 
 
 # Website-1
@@ -45,9 +38,12 @@ async def hello_world3(request: Request):
     return templates.TemplateResponse("website-3.html", {"request": request})
 
 
-@app.post("/api/url")
-async def scan_url(url: str):
-    return url
+@app.post("/api/url/{url}")
+def scan_url(url: str):
+    # retrieves/downloads images from a website
+    get_image_list(url)
+
+    return results
 
 
 if __name__ == "__main__":
