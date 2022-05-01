5f1ccedc07cbd8e3ae5fc10642846494fd6c9d57
Suyash Gupta <sgalpha01@gmail.com>
2022-05-01 16:17:51 +0530

    refactored code

---
+++ backend/image_downloader.py
@@ -0,0 +1,34 @@
+import urllib.request
+from pathlib import Path, PurePath
+
+import requests
+from bs4 import BeautifulSoup
+
+workdir = Path(__file__).resolve().parent
+imgdir = PurePath.joinpath(workdir, ".image_cache")
+Path(imgdir).mkdir(exist_ok=True)
+
+
+def getdata(url):
+    r = requests.get(url)
+    return r.text
+
+
+def get_image_list(url):
+    htmldata = getdata(url)
+    soup = BeautifulSoup(htmldata, "html.parser")
+    urls = []
+    for item in soup.find_all("img"):
+        urls.append(item["src"])
+    download_images_function(urls)
+
+
+def download_image(url, file_path, file_name):
+    full_path = file_path + file_name + ".png"
+    urllib.request.urlretrieve(url, full_path)
+
+
+def download_images_function(urls):
+    for url in urls:
+        file_name = str(id(url))
+        download_image(url, "imgdir", file_name)
